b5a1fb930da406cbd69b935d2357d9a7f0d13da9
renildoJr <110069785+renildoJr@users.noreply.github.com>
2022-09-29 22:13:41 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,36 +1,20 @@
 'use strict';
-const btnAddHora = document.querySelector(".add-hora");
-const btnFechaModal = document.querySelectorAll(".btn-fechar_janela");
-const btnConfirma = document.querySelectorAll(".btn-confirmar");
+const btnFechaModalHora = document.querySelector(".modal#horario-modal .btn-fechar_janela");
+const btnConfirmaHora = document.querySelector(".btn-confirmar#confirma-hora");
 const overlay = document.querySelector(".overlay");
 const modalHora = document.querySelector(".modal#horario-modal");
-const tabela = document.querySelector(".tabela-dr tbody");
-let trID = 0;
 
-const openModal = (modal, index, funcao) => {
-    modal.classList.add("mostrar");
+const abreModalHora = elemento => {
+    modalHora.classList.add("mostrar");
     overlay.classList.add("mostrar");
-    btnConfirma[index].addEventListener("click", () => {closeModal(modal); funcao()})
-    btnFechaModal[index].addEventListener("click", () => {closeModal(modal)})
+    btnConfirmaHora.addEventListener("click", ()=>{
+        const hora = document.querySelector(".modal .hora").value;
+        elemento.textContent=hora;
+    })
+    btnFechaModalHora.addEventListener("click", fecharModalHora);
 }
 
-const closeModal = modal => {
-    modal.classList.remove("mostrar");
+function fecharModalHora () {
+    modalHora.classList.remove("mostrar");
     overlay.classList.remove("mostrar");
 }
-
-const addHora = () => {
-    tabela.innerHTML+=`<tr class="tabelaHora" id="${trID}"></tr>`;
-    const tabelaHora = document.querySelectorAll(".tabelaHora");
-    const hora = document.querySelector(".modal .hora").value;
-
-    for(let i = 0; i < 8; i++ ){
-        i === 0? tabelaHora[trID].innerHTML+=`<td>${hora}</td>`: tabelaHora[trID].innerHTML+="<td>hello world</td>";
-    }
-    
-    console.log(tabelaHora)
-
-    trID++;
-}
-
-btnAddHora.addEventListener("click", ()=>{openModal(modalHora, 0, addHora)});
