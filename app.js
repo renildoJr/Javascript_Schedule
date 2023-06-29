2d79e2ab1e1829e30667c9da51e7c5f17ef67854
renildoJr <juninho952020@gmail.com>
2023-06-29 16:23:58 -0300

    New Logic

--- app.js
+++ app.js
@@ -1,56 +1,62 @@
 'use strict';
-
-// // Código Antigo 09/2022
-// const btnFechaModalHora = document.querySelector(".modal#horario-modal .btn-fechar_janela");
-// const btnConfirmaHora = document.querySelector(".btn-confirmar#confirma-hora");
-// const overlay = document.querySelector(".overlay");
-// const modalHora = document.querySelector(".modal#horario-modal");
-
-// const abreModalHora = elemento => {
-//     modalHora.classList.add("mostrar");
-//     overlay.classList.add("mostrar");
-//     btnConfirmaHora.addEventListener("click", ()=>{
-//         const hora = document.querySelector(".modal .hora").value;
-//         elemento.textContent = hora;
-//     })
-//     btnFechaModalHora.addEventListener("click", fecharModalHora);
-// }
-
-// function fecharModalHora () {
-//     modalHora.classList.remove("mostrar");
-//     overlay.classList.remove("mostrar");
-// }
-
-// novo Código - 06/2023
-
 // TAREFA
+// class Task {
+//     id;
+//     name;
+//     prior;
+//     category;
+//     desc;
+//     days = [];
+//     hours = [];
+//     done = [];
+//     justify = [];
+//     failed = [];
+// }
 
-class Task {
-    id;
-    name;
-    prior;
-    category;
-    desc;
-    days = [];
-    hours = [];
-    done = [];
-    justify = [];
-    failed = [];
+// function createTask() {
+//     const task = new Task();
+//     task.id = 12;
+//     task.name = 'play castevania';
+//     task.prior = 2; 
+//     task.category = 'Lazer';
+//     task.desc = 'blablalba';
+//     task.days = [1, 2, 3, 4, 5, 6];
+//     task.hours = [1030, 1123];
+
+//     // console.log(task)
+// }
+// createTask()
+
+const tabelasContainer = document.querySelector('.tabelas-container');
+
+const calendarMonths = ['january', 'february', 'march', 'april', 'may', 'junho', 'july', 'august', 'september', 'october', 'november', 'december']
+const calendarDays = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb']
+const lastDayCurrentMonth = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0 ).getDate();
+const firstDayCurrentMonth = new Date().getDay();
+const currentMonth = new Date().getMonth();
+
+const lastDayPreviousMonth = new Date(new Date().getFullYear(), currentMonth, 0 ).getDate();
+
+let lastDays = [];
+
+for(let day = lastDayPreviousMonth - 7; day <= lastDayPreviousMonth; day ++) {
+    lastDays.push(day)
 }
 
-function createTask() {
-    const task = new Task();
+lastDays = lastDays.splice(firstDayCurrentMonth)
 
-    task.id = 12;
-    task.name = 'play castevania';
-    task.prior = 2; 
-    task.category = 'Lazer';
-    task.desc = 'blablalba';
-    task.days = [1, 2, 3, 4, 5, 6];
-    task.hours = [1030, 1123];
-
-    console.log(task)
+for(let sem = 0, day = lastDays[0]; sem < 5; sem ++) {
+    const table = document.createElement('table');
+    const tr = document.createElement('th');
+    for(let i = 0; i < 7; i++) {
+        if(day < lastDays[lastDays.length - 1]) {
+            tr.innerHTML += `<th>${calendarDays[i]} ${day}</th>`;
+            day++;
+        }else {
+            day = 1
+            tr.innerHTML += `<th>${calendarDays[i]} ${day}</th>`;
+        }
+    }
+    table.appendChild(tr);
+    tabelasContainer.appendChild(table)
 }
-
-createTask()
-
