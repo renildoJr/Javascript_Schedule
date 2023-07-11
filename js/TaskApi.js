7f6700314cb756612ef54d28e6891e215296c8cd
renildoJr <juninho952020@gmail.com>
2023-07-10 23:38:39 -0300

    Changes

--- js/TaskApi.js
+++ js/TaskApi.js
@@ -2,11 +2,11 @@ export class Task {
     defaultColor = '#F2F2F2';
 
     static getTasks() {
-        const tasks = localStorage.getItem('Routine');
+        const tasks = localStorage.getItem('schedi');
         return JSON.parse(tasks) || [];
     }
 
-    static newTask(name = 'unknown', horarios = [], dias = [], status = [], color = this.defaultColor, updatedId = null) {
+    static newTask(name = 'unknown', hour = 1000, days = [], status = [], color = this.defaultColor, updatedId = null) {
         const tasks = this.getTasks();
         // validar para ver se o nome da tarefa já existe no localstorage
         // validar para ver se há confilito de horarios + dias com outras tarefas existentes
@@ -14,15 +14,14 @@ export class Task {
         const newTask = {
             id: updatedId !== null ? updatedId : Math.round(Math.random() * 1000),
             name,
-            horarios,
-            dias,
+            hour,
+            days,
             status,
             color
         };
 
         tasks.push(newTask);
         save(tasks);
-
         return newTask;
     }
 
@@ -32,11 +31,11 @@ export class Task {
         return task;
     }
 
-    static editTask(id, name, horarios, dias, status, color) {
+    static editTask(id, name, hour, days, status, color) {
         // remove task
         this.removeTask(id);
         // add new
-        return this.newTask(name, horarios, dias, status, color, id);
+        return this.newTask(name, hour, days, status, color, id);
 
     }
 
@@ -45,7 +44,7 @@ export class Task {
         const currentTask = tasks.find(task => task.id === id);
 
         if(!currentTask) {
-            throw Error('Task não encontrada');
+            throw Error('Task not found!');
         }
 
         tasks.splice(tasks.indexOf(currentTask), 1);
@@ -56,6 +55,6 @@ export class Task {
 }
 
 function save(data) {
-    return localStorage.setItem('Routine', JSON.stringify(data))
+    return localStorage.setItem('schedi', JSON.stringify(data))
 }
 
--- js/app.js
+++ js/app.js
@@ -1,175 +1,176 @@
+// localStorage.clear()
+// Task.newTask('Go Bowling', 830, [18, 11, 15], [], 'orangered');
+// Task.newTask('Watch Anime', 925, [10, 11, 23, 12], [], '#000000') 
+// Task.newTask('watch tv', 1200, [7, 8, 12, 23], [], '#ab4e91');
+// Task.newTask('play game', 2300, [3, 6, 15, 18], [], '#8ef3f3');
+// Task.newTask('sleep', 100, [2, 5, 7, 8, 9, 16], [], '#70cbba');
+
 'use strict';
 import { Task } from "./TaskApi.js";
 const tasks = Task.getTasks();
 const date = new Date();
 const mesAtual = new Date(date.getFullYear(), date.getMonth()).getMonth();
-const ultimoDia = new Date(date.getFullYear(), mesAtual + 1, 0).getDate();
-const primeiroDiaSemana = new Date(date.getFullYear(), mesAtual).getDay();
-const diasSemana = [[], [], [], [], [], [], []];
-const hoje = date.getDate();
+const lastDay = new Date(date.getFullYear(), mesAtual + 1, 0).getDate();
+const firstDayOfWeek = new Date(date.getFullYear(), mesAtual).getDay();
+const weeks = [[], [], [], [], [], [], []];
+const today = date.getDate();
 
-PROVresetTasks()
-
-for(let d = 1, dia = primeiroDiaSemana; d <= ultimoDia; d++) {
-    diasSemana[dia].push({dia: d, semana: numeroSemana(d) , tasks: []})
-    if(dia > diasSemana.length - 2) {
+for(let d = 1, dia = firstDayOfWeek; d <= lastDay; d++) {
+    weeks[dia].push({dia: d, semana: getWeekNumber(d) , tasks: []})
+    if(dia > weeks.length - 2) {
         dia = 0;
     }else {
         dia ++;
     }
 }
 
-tasks.forEach(task => {
-    task.dias.forEach(dia => {
-        for(const diaSem of diasSemana) {
-            const objDia = diaSem.find(obj => obj.dia === dia);
-            if(objDia) {
-                objDia.tasks.push(task.id);
+updateDailyStatus();
+
+// Salva as o ID das tarefas no array tasks (da variável weeks) correspondente aos dias de cada tarefa
+// ATENÇÃO: Adaptar esse código para uma só Task, quando for criada ou modificada
+// PERF: Este código só deverá ser executado uma só vez a cada task criada ou modificada.
+tasks.forEach(task => 
+   task.days.forEach(day => {
+        for(const week of weeks) {
+            const weekDay = week.find(obj => obj.dia === day);
+            if(weekDay) {
+                weekDay.tasks.push(task.id);
             }
         }
+   })
+);
+
+renderCalendarHTML();
+
+ // // Renderização HTML do calendário (Semana atual)
+ function renderCalendarHTML() {
+    const semana = listWeeks(getWeekNumber(today));
+    const calendarDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const tabela = document.querySelector('.tabelas-container table');
+    const thead = document.createElement('tr');
+    const lastDayMesPassado = new Date(date.getFullYear(), mesAtual, 0).getDate();
+    const tasksSemanaAtual = new Set();
+    let ultimosDiasMesPassado = [];
+    let diaSMesSeguinte = 1; // Melhorar isso mas tarde
+    let novoIndex = 0;
+    let tasksHours = [];
+    let index = 7;
+    
+    for(let i = 0, dia = lastDayMesPassado - 6; i < 7; i++) {
+        ultimosDiasMesPassado.push(dia);
+        dia++;
+    }
+    
+    ultimosDiasMesPassado = ultimosDiasMesPassado.splice(listWeeks(0).length);
+    thead.innerHTML = `<th></th>`;
+    
+    calendarDays.map((dia, index) => {
+        let data;
+        if(semana[0].semana === 0 && index !== ultimosDiasMesPassado.length) {
+            data = ultimosDiasMesPassado[index];
+        }else {
+            const objSemana = semana[novoIndex];
+            if(objSemana) {
+                data = objSemana.dia; //CORRIGIR ESTE ERRO AQUI
+    
+            }else {
+                data = diaSMesSeguinte; //CORRIGIR ESTE ERRO AQUI
+                diaSMesSeguinte++;
+            }
+            novoIndex ++;
+        }
+        // Verifica se a data é igual á data atual para aplicar a classe "today"
+        thead.innerHTML+=`<th class="date${data === today ? ' today' : ''}">${dia} ${data}</th>`;
     })
-});
+    
+    tabela.appendChild(thead)
+        
+    semana.forEach(obj => {
+        obj.tasks.forEach(taskId => tasksSemanaAtual.add(taskId));
+    })
 
-// Renderização HTML do calendário (Semana atual)
-const diasCalendario = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'];
-const tabela = document.querySelector('table');
-const thead = document.createElement('tr');
+    // Acessar horários dos itens
+    tasksSemanaAtual.forEach(taskId => {
+        const task = Task.getTask(taskId);
+        tasksHours.push(task.hour)
+    });
 
-// Renderização HTML do calendário (Semana atual)
-const ultimoDiaMesPassado = new Date(date.getFullYear(), mesAtual, 0).getDate();
+    tasksHours = tasksHours.sort(((a, b) => a - b));
 
-let ultimosDiasMesPassado = [];
+    // renderizar as rows HTML de horarios
+    tasksHours.forEach(hora => {
+        const tr = document.createElement('tr');
+        tr.innerHTML = `<td class="hora">${formatHours(hora)}</td>`;
 
-for(let i = 0, dia = ultimoDiaMesPassado - 6; i < 7; i++) {
-    ultimosDiasMesPassado.push(dia);
-    dia++;
+        if(semana.length < index ) {
+            for(let i = 0; i < ultimosDiasMesPassado.length; i++)
+            tr.innerHTML+=`<td></td>`;
+            index++;
+        }
+
+        semana.forEach((obj, index) => {
+            // Descobrir a task correspondente ao do horário iterado
+            const td = document.createElement('td');
+            let taskAtual = false;
+            let taskName = '';
+            let done = null;
+            let notDone = null;
+
+            for(const taskId of obj.tasks) {
+                const task = Task.getTask(taskId);
+                if(task.hour === hora){
+                    taskAtual = task;
+                    break;
+                }
+            }
+            
+            td.setAttribute('class', 'task');
+         
+            if(taskAtual) {
+                taskName = taskAtual.name;
+                done = taskAtual.status.find(status => (status.day === semana[index].dia) && (status.done === true)) || null;
+                notDone = taskAtual.status.find(status => (status.day === semana[index].dia) && (status.done === false)) || null;
+                td.setAttribute('style', `background: ${taskAtual ? taskAtual.color : ''}`);
+                if(done) {
+                    td.setAttribute('style', `background: #21d942`);
+                }else if(notDone) {
+                    td.setAttribute('style', 'background: red');
+                }
+            }
+
+            td.innerHTML = taskName;
+            td.addEventListener('click', ()=> {taskClick(obj.dia, taskAtual)});
+            tr.appendChild(td);
+        })
+
+        tabela.appendChild(tr);
+    });
+
 }
 
-ultimosDiasMesPassado = ultimosDiasMesPassado.splice(listaSemana(0).length);
-
-// limitar até 5
-const semana = listaSemana(numeroSemana(hoje));
-
-thead.innerHTML = `<th></th>`;
-
-let diaSMesSeguinte = 1; // Melhorar isso mas tarde
-let novoIndex = 0;
-diasCalendario.map((dia, index) => {
-    let data;
-    if(semana[0].semana === 0 && index !== ultimosDiasMesPassado.length) {
-        data = ultimosDiasMesPassado[index];
-    }else {
-        const objSemana = semana[novoIndex];
-        if(objSemana) {
-            data = objSemana.dia; //CORRIGIR ESTE ERRO AQUI
-
-        }else {
-            data = diaSMesSeguinte; //CORRIGIR ESTE ERRO AQUI
-            diaSMesSeguinte++;
-        }
-        novoIndex ++;
-    }
-    // Verifica se a data é igual á data atual para aplicar a classe "hoje"
-    thead.innerHTML+=`<th class="${data === hoje ? 'hoje' : ''}">${dia} ${data}</th>`;
-})
-
-tabela.appendChild(thead)
-
-const tasksSemanaAtual = new Set();
-
-semana.forEach(obj => {
-    obj.tasks.forEach(taskId => tasksSemanaAtual.add(taskId));
-})
-
-
-let horariosSemanaAtual = [];
-
-// Acessar horários dos itens
-tasksSemanaAtual.forEach(taskId => {
-    const task = Task.getTask(taskId);
-    task.horarios.forEach(hora => {horariosSemanaAtual.push(hora)})
-});
-
-let index = 7;
-horariosSemanaAtual = horariosSemanaAtual.sort(((a, b) => a - b));
-
-// renderizar as rows HTML de horarios
-horariosSemanaAtual.forEach(hora => {
-    const tr = document.createElement('tr');
-    tr.innerHTML = `<td class="hora">${formatHoras(hora)}</td>`;
-
-    if(semana.length < index ) {
-        for(let i = 0; i < ultimosDiasMesPassado.length; i++)
-        tr.innerHTML+=`<td></td>`;
-        index++;
-    }
-
-    semana.forEach((obj, index) => {
-        // Descobrir a task correspondente ao do horário iterado
-        let taskAtual = false;
-        let taskName = '';
-        let done = null;
-        let notDone = null;
-
-        for(const taskId of obj.tasks) {
-            const task = Task.getTask(taskId);
-            if(task.horarios.find(horario => horario === hora)){
-                taskAtual = task;
-                break;
-            }
-        }
-
-        if(taskAtual) {
-            taskName = taskAtual.name;
-            done = taskAtual.status.find(status => (status.dia === semana[index].dia) && (status.done === true)) || null;
-            notDone = taskAtual.status.find(status => (status.dia === semana[index].dia) && (status.done === false)) || null;
-        }
-
-        const td = document.createElement('td');
-        td.setAttribute('class', 'task');
-        td.setAttribute('style', `background: ${taskAtual ? taskAtual.color : ''}`);
-
-        if(done) {
-            td.setAttribute('style', `background: #21d942`);
-        }else if(notDone) {
-            td.setAttribute('style', 'background: red');
-        }
-
-        td.innerHTML = taskName;
-        td.addEventListener('click', ()=>{teste(obj.dia, taskAtual)});
-        tr.appendChild(td);
-    })
-
-    tabela.appendChild(tr);
-});
-
-
-// console.log(tasks)
-
-// if(hoje === 1) {
-//     //1. armazenar relatório do mês quando localStorage hoje === 1
+// if(today === 1) {
+//     //1. armazenar relatório do mês quando localStorage today === 1
 
 //     //2. zera o array da propriedade status das tasks após o envio do relatório mensal
-//     tasks.forEach(task => Task.editTask(task.id, task.name, task.horarios, task.dias, task.status = [{dia: hoje, done: null}], task.color));
+//     tasks.forEach(task => Task.editTask(task.id, task.name, task.horarios, task.dias, task.status = [{dia: today, done: null}], task.color));
 // }
 
 
-// localStorage.clear()
-// Task.newTask('watch tv', [1200], [7, 8, 12, 23], [], '#ab4e91');
-// Task.newTask('play game', [2300], [3, 6, 15, 18], [], '#8ef3f3');
-// Task.newTask('sleep', [100], [2, 5, 7, 8, 9, 16], [], '#70cbba');
-
 // FUNÇÕES
 
-function PROVresetTasks() {
+function openModal(edit) {
+    const modal = document.createElement('div');
+}
+
+function updateDailyStatus() {
     // OBS: ESTE CÓDIGO SÓ DEVERÁ SER EXECUTADO UMA SÓ VEZ, E SEMPRE QUANDO FOR ADICIONADA UMA NOVA TASK
     // ESTE CÓDIGO DEVERÁ SER ADAPITADO PARA UMA TASK SÓ (A NOVA TASK CRIADA), AO INVÉS DE TODAS
     // Adiciona status null relativo aos dias nas novas tasks
     // tasks.forEach(task => {
-    //     task.dias.forEach(dia => {
-    //         if(!task.status.find(stat => stat.dia === dia && stat.done === false)) {
-    //             task.status.push({dia: dia, done: null});
-    //             Task.editTask(task.id, task.name, task.horarios, task.dias, task.status, task.color);
+    //     task.days.forEach(day => {
+    //         if(!task.status.find(stat => stat.day === day && stat.done === false)) {
+    //             task.status.push({day: day, done: null});
+    //             Task.editTask(task.id, task.name, task.hour, task.days, task.status, task.color);
     //         }else {
     //             console.log('já existe')
     //         }      
@@ -180,24 +181,24 @@ function PROVresetTasks() {
     tasks.forEach(task => {
         const newStatus = [];
         task.status.forEach((status) => {
-            if((status.dia < hoje && status.done === null)) {
-                newStatus.push({dia: status.dia, done: false});
+            if((status.day < today && status.done === null)) {
+                newStatus.push({day: status.day, done: false});
             }else {
                 newStatus.push(status)
             }
         });
-        Task.editTask(task.id, task.name, task.horarios, task.dias, newStatus, task.color);
+        Task.editTask(task.id, task.name, task.hour, task.days, newStatus, task.color);
     });
 }
 
-function teste(dia, task) {
+function taskClick(dia, task) {
     if(task) {
-        const statusExiste = task.status.find(stat => (stat.dia === dia) && stat.done !== null);
+        const statusExiste = task.status.find(stat => (stat.day === dia) && stat.done !== null);
         if(statusExiste) {
             console.log('Não é possível marcar esta tarefa novamente');
-        }else if(dia === hoje) {
-            task.status.push({dia: hoje, done: true}); 
-            Task.editTask(task.id, task.name, task.horarios, task.dias, task.status, task.color);
+        }else if(dia === today) {
+            task.status.push({day: today, done: true}); 
+            Task.editTask(task.id, task.name, task.hour, task.days, task.status, task.color);
             return location.reload();
         }
     }else {
@@ -206,9 +207,9 @@ function teste(dia, task) {
 
 }
 
-function listaSemana(numSem = 0) {
+function listWeeks(numSem = 0) {
     const semana = [];
-    for(const array of diasSemana) {
+    for(const array of weeks) {
         const obj = array.find(obj => obj.semana === numSem);
         if(obj) {
             semana.push(obj);
@@ -217,24 +218,35 @@ function listaSemana(numSem = 0) {
     return semana;
 }
 
-// Esta função descobre a semana da data especificada
-function numeroSemana(dia) {
-    const offsetInicio = (primeiroDiaSemana + 6) % 7; // Offset para o primeiro dia da semana
+// Esta função descobre a semana da data especificada por parâmetro
+function getWeekNumber(dia) {
+    const offsetInicio = (firstDayOfWeek + 6) % 7; // Offset para o primeiro dia da semana
     const ajusteDia = dia + offsetInicio ;
     return Math.floor(ajusteDia / 7);
 }
 
-function formatHoras(num) {
+// Validar futuramente caso o input seja: maior que 60, negativo, hora >= 2400, ... 
+function formatHours(num) {
+    let mask = '00:00';
     num = String(num);
     
+    if(num.length < 3) {
+        return "00:"+num
+    }
+
     if(num.length === 3) {
         return  '0'+num.charAt(0)+':'+num.substring(1);
     }
 
-    let mask = '00:00';
     for(let i = 0; i < num.length; i++) {
         mask = mask.replace('0', num[i])
     }
 
     return mask;
 }
+
+/**
+ * Adicionar funcionalidades CRUD para o sistema
+ * Organizar/Refatorar código
+ * Criar interface mais amigável
+ */
